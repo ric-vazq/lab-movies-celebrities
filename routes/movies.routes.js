8141fcf649b9ca5bbc0bfdc97184b11e82e40f7f
Ricardo Vazquez <rickyrabv@gmail.com>
2023-12-10 15:06:50 +0100

    lab completed, bonus open

---
+++ routes/movies.routes.js
@@ -0,0 +1,59 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model")
+
+// all your routes here
+router.get("/movies/create", (req, res, next) => {
+    Celebrity.find()
+        .then(celebrities => res.render("movies/new-movie", {celebrities}))
+        .catch(err => next(err)); 
+})
+router.post("/movies/create", (req, res, next) => {
+    const { title, genre, plot, cast } = req.body; 
+    Movie.create({ title, genre, plot, cast })
+    .then(() => res.redirect("/movies"))
+    .catch(err => {
+        next(err);
+        return res.render("movies/new-movie")
+    })
+})
+router.get("/movies", (req, res, next) => {
+    Movie.find()
+    .then(movies => res.render("movies/movies", {movies}))
+    .catch(err => next(err))
+})
+router.get("/movies/:id", (req, res, next) => {
+    const { id } = req.params;
+    Movie.findById(id)
+    .populate('cast')
+    .then(movie => res.render("movies/movie-details", movie))
+    .catch(err => next(err))
+})
+router.post("/movies/:id/delete", (req, res, next) => {
+    const { id } = req.params;
+    Movie.findByIdAndDelete(id)
+        .then(() => res.redirect("/movies"))
+        .catch(err => next(err))
+})
+router.get("/movies/:id/edit", (req, res, next) => {
+    const { id } = req.params; 
+    let movieFound; 
+    Movie.findById(id)
+    .then(movie => {
+        movieFound = movie;
+        return Celebrity.find()
+    })
+    .then(allCelebs => {
+        return res.render("movies/edit-movie", { celebrities: allCelebs, movie: movieFound })
+    })
+    .catch(err => next(err))
+})
+router.post("/movies/:id/edit", (req, res, next) => {
+    const { id } = req.params;
+    const { title, genre, plot, cast } = req.body;
+    Movie.findByIdAndUpdate(id, { title, genre, plot, cast }, { new: true })
+    .then(x => res.redirect(`/movies/${id}`))
+    .catch(err => next(err))
+})
+module.exports = router;
